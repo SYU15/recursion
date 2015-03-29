1910fe3d7172d406bf98bfbaffef9cc0445f6282
Sarah Yu <sarah.m.yu88@gmail.com>
2015-03-29 15:28:43 -0800

    Stuck on getElementsByClassName, returns correct HTML corresponding to class being search for, but returns html inside of string instead of just html. Will come back to it later.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,18 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+  var hasClass = [];
+  var childNodes = document.body.childNodes;
+  var i = childNodes.length -1;
+  var recursiveCheck = function(n) { 
+    if (n === 0) {
+      return hasClass;
+    } else {
+      if (childNodes[n].nodeType === 1 && childNodes[n].classList.contains(className)) {
+        hasClass.push(childNodes[n].outerHTML);
+      }
+      return recursiveCheck(n-1);
+    }
+  }
+  recursiveCheck(i);
 };
