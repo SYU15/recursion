92cda0f85b85b32f621c3de2ac4f473acda1ce55
Sarah Yu <sarah.m.yu88@gmail.com>
2015-03-29 23:53:01 -0800

    Got stringifyJSON(); to convert simple data structures into JSON, still experiencing bugs with arrays and objects. Will hopefully fix in the next commit.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringified = "";
+  if (typeof obj === 'string') {
+    stringified += '"'+ obj +'"';
+  } else if (typeof obj === 'number') {
+    stringified += obj.toString();
+  } else if (typeof obj === 'boolean') {
+    stringified += obj.toString();
+  } else if (obj === null) {
+    stringified += 'null';
+  }
+  else if(typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      stringified += '[';
+      for (var i; i < obj.length; i++) {
+        stringifyJSON(obj[i]);
+        if (i < obj.length -1) {
+          stringified += ',';
+        }
+      }
+      stringified +=']';
+    } else {
+      stringified += '{';
+      for (var key in obj) {
+        stringifyJSON(key);
+        stringified += ":";
+        stringifyJSON(obj[key]);
+        stringified += ",";
+      }
+      stringified.slice(0,-1);
+      stringified += '}';
+    }
+  }
+  return stringified;
 };
